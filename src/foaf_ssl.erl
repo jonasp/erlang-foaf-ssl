2858c771410643569a4881d35d78708a626b4701
Jonas Pollok <jonas.p@gmail.com>
2010-08-11 14:18:46 +0200

    fixed module declaration

--- src/foaf_ssl.erl
+++ src/foaf_ssl.erl
@@ -8,7 +8,9 @@
 % distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 % WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 % License for the specific language governing permissions and limitations under
-% the License.-module(foaf_ssl).
+% the License.
+
+-module(foaf_ssl).
 
 -export([verify_user/1]).
 
